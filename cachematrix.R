5b04d1b149ad0df76eb5191317a4d754a2fb2eaa
Narayana Kiran Bugata <kiranbugata@yahoo.com>
2014-07-26 00:48:46 -0700

    Created R script that creates Cached Matrix, which will cache a matrix and additionally compute and cache its inverse along with the original

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file contains two functions 
 
-## Write a short comment describing this function
+## The funtion "makeCacheMatrix" is designed to create two global variables,
+## variable "origMatrix" to cache a Matrix and variable "InvMatrix" to 
+## cache the Inverse of the same Matrix. 
+## This function returns a list of four functions, two of which are designed
+## to set and get value of "origMatrix" variable and the other two are 
+## desgined to set and get the value of "InvMatrix" variable
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## Cache the matrix supplied to the function into "origMatrix" variable
+  origMatrix <<- x 
+  ## set "invMatrix" to NULL as Inverse is not computed at this time
+  invMatrix <- NULL
+  
+  ## This function is designed to assign a matrix directly to "origMatrix"
+  setOrigMatrix <- function(mat) {
+    ## Cache the matrix supplied to the function into "origMatrix" variable
+    origMatrix <<- mat
+    ## set "invMatrix" to NULL as Inverse is not computed at this time
+    invMatrix <<- NULL
+  }
+  
+  ## This function is designed to return "origMatrix"
+  getOrigMatrix <- function() origMatrix
+  
+  ## This function is designed to cache a supplied inverse matrix 
+  ## directly into "invMatrix" variable
+  setInvMatrix <- function(invMat) invMatrix <<- invMat
+  
+  ## This function is designed to return "invMatrix"
+  getInvMatrix <- function() invMatrix
+  
+  ## Return the list containing four functions, two each to manipulate 
+  ## "origMatrix" and "invMatrix" variables respectively
+  list(setOrigMatrix = setOrigMatrix, getOrigMatrix = getOrigMatrix,
+       setInvMatrix = setInvMatrix,
+       getInvMatrix = getInvMatrix)
+  
+} #End of makeCacheMatrix
 
-}
 
-
-## Write a short comment describing this function
+## The function "cacheSolve" takes a "Cached Vector" and checks if it  
+## contains the inverse of original matrix. 
+## If the inverse exist (and the original matrix has not changed), 
+## this function retrieves the inverse from the Cache.
+## If the inverse does not exist, it computes and inserts the inverse
+## back into the supplied "Cached Vector"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  
+  ## Get "invMatrix" variable of the supplied Cached vector
+  invMatrix <- x$getInvMatrix()
+  
+  ## Check if "invMatrix" is not Null to verify if inverse is cached. 
+  ## If it is cached then return the cached value.
+  if(!is.null(invMatrix)) {
+    message("getting cached data")
+    return(invMatrix)
+  }
+  
+  ## If inverse does not exist then get the original matrix using 
+  ## "getOrigMatrix" function
+  origMatrix1 <- x$getOrigMatrix()
+  ## Solve the original matrix to compute its inverse
+  invMatrix <- solve(origMatrix1, ...)
+  ## Set the computed inverse into the supplied Cached Vector
+  x$setInvMatrix(invMatrix)
+  ## Return the Inverted matrix
+  invMatrix
+  
+} #End of cacheSolve
